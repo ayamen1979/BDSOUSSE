8487d2c15d76363d62c841bb0435c796c3753b99
ayamen1979 <escritorio.c.civil.fisaclizacao@gmail.com>
2026-05-06 21:00:17 +0100

    Add files via upload
    Initial project upload

---
+++ data/webconstruction_industrielle_13.js
@@ -0,0 +1,1 @@
+var json_webconstruction_industrielle_13 = {"type":"FeatureCollection","name":"webconstruction_industrielle_13","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ data/webvegetation_lineare_3.js
@@ -0,0 +1,1 @@
+var json_webvegetation_lineare_3 = {"type":"FeatureCollection","name":"webvegetation_lineare_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
